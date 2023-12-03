cddc5b823eaf13fa9e5c3be680fc6fbdde85d207
Horacio Trujillo <hftrujillo@loyola.edu>
2023-12-02 20:42:41 -0500

    loginView visuals done

--- RecWell App/RecWell App/LoginView.swift
+++ RecWell App/RecWell App/LoginView.swift
@@ -1,69 +1,103 @@
 import SwiftUI
 
 struct LoginView: View {
-    
-    //@ObservedObject var viewModel : ViewModel
-    
+
     @State var username: String = ""
     @State var password: String = ""
     
     var body: some View {
         
         // (red: 0.5, green: 0.2, blue: 0.6)
-        
-        ZStack {
+        NavigationView {
             
-            Color(red: 0.369, green: 0.369, blue: 0.369)
-            
-            RoundedRectangle(cornerRadius: 30, style: .continuous)
-                .foregroundStyle(.linearGradient(colors: [.gray, .gray], startPoint:
-                        .topLeading, endPoint: .bottomTrailing))
-                .frame(width: 1000, height: 400)
-                .rotationEffect(.degrees(135))
-                .offset(y: -350)
-            
-            VStack {
+            ZStack {
                 
-                // Logo / welcome greeting
-                //Image("RecWell Logo 2").frame(width: 2, height: 2)
-                Text("Welcome to RecWell")
+                // BOTTOM COLOR
+                Color(red: 0.369, green: 0.369, blue: 0.369)
+                
+                RoundedRectangle(cornerRadius: 30, style: .continuous)
+                
+                // TOP COLOR
+                    .fill(Color(red: 0.42, green: 0.42, blue: 0.42))
+                    .frame(width: 1000, height: 400)
+                    .rotationEffect(.degrees(135))
+                    .offset(y: -350)
+                
+                VStack {
                     
-
-                // Textfield for username
-                TextField("Username", text: $username)
-                    .foregroundColor(.white)
-                    .textFieldStyle(.plain)
-                    .placeholder(when: username.isEmpty) {
-                        Text("Username")
-                            .foregroundColor(.white)
+                    // Logo / welcome greeting
+                    Text("Welcome to RecWell")
+                        .font(.system(size: 30))
+                        .foregroundColor(.white)
+                        .bold()
+                    
+                    // Textfield for username
+                    TextField("Username", text: $username)
+                        .foregroundColor(.white)
+                        .textFieldStyle(.plain)
+                        .placeholder(when: username.isEmpty) {
+                            Text("Username")
+                                .foregroundColor(.white)
+                                .bold()
+                        }
+                    
+                    Rectangle()
+                        .frame(width: 350, height: 1)
+                        .foregroundColor(.white)
+                    
+                    // Textfield for password
+                    SecureField("Password", text: $password)
+                        .foregroundColor(.white)
+                        .textFieldStyle(.plain)
+                        .placeholder(when: password.isEmpty) {
+                            Text("Password")
+                                .foregroundColor(.white)
+                                .bold()
+                        }
+                    
+                    Rectangle()
+                        .frame(width: 350, height: 1)
+                        .foregroundColor(.white)
+                    
+                    // Login button
+                    Button {
+                        
+                        // ACTION HERE
+                        //NavigationLink(destination: ContentView(viewModel: ViewModel())) {}
+                        
+                    } label: {
+                        Text("Login")
                             .bold()
-                    }
-                Rectangle()
-                    .frame(width: 350, height: 1)
-                    .foregroundColor(.white)
-                
-                // Textfield for password
-                SecureField("Password", text: $password)
-                    .foregroundColor(.white)
-                    .textFieldStyle(.plain)
-                    .placeholder(when: password.isEmpty) {
-                        Text("Password")
+                            .frame(width: 200, height: 40)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                    .fill(Color(red: 0.22, green: 0.463, blue: 0.114)))
                             .foregroundColor(.white)
-                            .bold()
                     }
-                
-                Rectangle()
-                    .frame(width: 350, height: 1)
-                    .foregroundColor(.white)
-                
-                // Login button
-                
-                // Sign up button
-                
+                    
+                    // Sign Up button
+                    Text("Dont have an acccount?")
+                        .foregroundColor(.white)
+                        .bold()
+                    
+                    Button {
+                        
+                        // ACTION HERE
+                        
+                    } label: {
+                        Text("Sign up")
+                            .bold()
+                            .frame(width: 200, height: 40)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                    .fill(Color(red: 0.22, green: 0.463, blue: 0.114)))
+                            .foregroundColor(.white)
+                    }
+                }
+                .frame(width: 350)
             }
-            .frame(width: 350)
+            .ignoresSafeArea()
         }
-        .ignoresSafeArea()
     }
 }
 
@@ -83,3 +117,11 @@ extension View {
     }
 }
 
+/*
+    .navigationBarItems(trailing: HStack {
+        
+        NavigationLink(destination: viewTasks(taskList: $taskList)) {
+            
+            Text("View Tasks").padding()
+        }
+*/
